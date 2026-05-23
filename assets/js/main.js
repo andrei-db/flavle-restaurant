9099ccc60229c89702cd007b217b0bbcb8e15a6d
Daraban Andrei <andrei.daraban70@yahoo.com>
2026-05-23 15:21:28 +0300

    feat: add sticky header scroll state

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,13 @@ revealElements.forEach((element) => {
     element.classList.add("reveal-section");
     observer.observe(element);
 });
+
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 80) {
+        header?.classList.add("is-scrolled");
+    } else {
+        header?.classList.remove("is-scrolled");
+    }
+});
